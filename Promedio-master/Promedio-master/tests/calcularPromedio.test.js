9bcaefaa4184e31f1d67bcf84b5a857baac7edef
Sandoval6 <127150564+Sandoval6@users.noreply.github.com>
2024-09-25 11:32:21 -0600

    Modificación en calcularPromedio.test.js

--- Promedio-master/Promedio-master/tests/calcularPromedio.test.js
+++ Promedio-master/Promedio-master/tests/calcularPromedio.test.js
@@ -11,3 +11,19 @@ test('Promedio de números corregido', () => {
     expect(calcularPromedioCorregido([10, 20, 30, 40, 50])).toBe(30);
     expect(calcularPromedioCorregido([])).toBe(0); // Corrección: debería devolver 0
 });
+//correcion
+const { calcularPromedioDefecto, calcularPromedioCorregido } = require('../src/calcularPromedio');
+
+test('Promedio de números con defecto', () => {
+    expect(calcularPromedioDefecto([10, 20, 30, 40, 50])).toBe(30);
+    
+    // Debería lanzar un error cuando el array esté vacío
+    expect(() => calcularPromedioDefecto([])).toThrow('Array vacío no permitido'); // Deberías personalizar el mensaje si es necesario
+});
+
+test('Promedio de números corregido', () => {
+    expect(calcularPromedioCorregido([10, 20, 30, 40, 50])).toBe(30);
+    
+    // En este caso, debería devolver 0 para un array vacío
+    expect(calcularPromedioCorregido([])).toBe(0);
+});
